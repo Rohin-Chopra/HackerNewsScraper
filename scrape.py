33c78f7932563330088965636ae63be63a5ad852
Rohin Chopra <58929990+Rohin1212@users.noreply.github.com>
2020-05-21 23:19:03 +1000

    Refactored Version
    Changed Variable Names and added support for receiving arguments for the CLI.

--- scrape.py
+++ scrape.py
@@ -1,17 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pprint
-
-response = requests.get('https://news.ycombinator.com/news')
-response2 = requests.get('https://news.ycombinator.com/news?p=2')
-
-soup =  BeautifulSoup(response.text, 'html.parser')
-links = soup.select('.storylink')
-subtext = soup.select('.subtext')
-
-soup2 =  BeautifulSoup(response2.text, 'html.parser')
-links2 = soup2.select('.storylink')
-subtext2 = soup2.select('.subtext')
+import sys
 
 def sort_stories_by_votes(hnlist):
     return sorted(hnlist, key = lambda k: k['votes'], reverse=True)
@@ -29,16 +19,19 @@ def create_cus_hn(links, subtext):
     return sort_stories_by_votes(hn)
 
 
-page1 = create_cus_hn(links, subtext)
-page2 = create_cus_hn(links2, subtext2)
 
 def response_creator(num_pages):
-    response_arr = []
+    response_arr = [] 
     response_arr.append(requests.get('https://news.ycombinator.com/news'))
-    for i in range(2, num_pages + 1):
-        responses = requests.get('https://news.ycombinator.com/news?p='+str(i))
-        response_arr.append(responses)
-    soup_creator(response_arr)
+    if num_pages < 1:
+        print('Number of pages has to be greater than 1.')
+    elif num_pages == 1:
+        soup_creator(response_arr)
+    elif num_pages > 1:
+        for i in range(2, num_pages + 1):
+            responses = requests.get('https://news.ycombinator.com/news?p='+str(i))
+            response_arr.append(responses)
+        soup_creator(response_arr)
   
 
 def soup_creator(response_arr):
@@ -51,13 +44,20 @@ def soup_creator(response_arr):
     multiple_create_cus_hn(huge2_arr)    
 
 def multiple_create_cus_hn(arr):
-    another_arr = []
+    cus_hn_arr = []
+    super_cus_hn_arr = []
     for item in arr:
-        another_arr.append(create_cus_hn(item['links'], item['subtext']))
-    super_arr = another_arr[0] + another_arr[1]  +  another_arr[2]  +  another_arr[3]  +  another_arr[4]
-    super_arr = sort_stories_by_votes(super_arr)     
-    pprint.pprint(super_arr)   
-print(response_creator(5))
+        cus_hn_arr.append(create_cus_hn(item['links'], item['subtext']))
+    for i in range(len(cus_hn_arr)):
+        super_cus_hn_arr += cus_hn_arr[i]
+    super_cus_hn_arr = sort_stories_by_votes(super_cus_hn_arr)
+    pprint.pprint(super_cus_hn_arr)
 
+
+def main():
+    num_pages = sys.argv[1]
+    response_creator(int(num_pages))
+
+main()
 
 
